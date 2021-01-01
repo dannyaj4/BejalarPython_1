87a127a9c37efa36d7fcae51693bc6016067824f
Danny A. J <63944195+dannyaj4@users.noreply.github.com>
2021-01-01 15:27:23 +0700

    Add files via upload

---
+++ Belajar Python - 1/main.py
@@ -0,0 +1,11 @@
+import time
+start_time = time.time()
+
+#hello world
+a = 10 #ini adalah comment
+
+print(time.time() - start_time, "detik")
+
+# Compile python -m py_compile filename.py
+# Compile membuat python lebih cepat execute 
+# python tipe interprete dari source code.
