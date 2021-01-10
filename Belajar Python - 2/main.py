eb91353b53e416b1b3c6f726b48ce23647b9d2f7
Danny A. J <63944195+dannyaj4@users.noreply.github.com>
2021-01-10 21:35:48 +0700

    Add files via upload

---
+++ Belajar Python - 2/main.py
@@ -0,0 +1,28 @@
+# Variable adalah tempat penyimpanan data
+
+# Menaruh / Assignment nilai, tidak ada deklarasi
+
+a = 10 
+x = 5
+panjang = 1000
+
+#pemanggilan pertama
+print("Nilai a =", a)
+
+print("Nilai x =", x)
+print("Nilai panjang =", panjang)
+
+# Penamaan
+panjang_x = 15 #boleh dengan menggunakan underscore
+juta10 = 1000000 #angka tidak boleh di depan
+nilaiZ = 17.5 #ini boleh digunakan
+
+#pemanggilan kedua
+print("Nilai a =", a)
+a = 7 
+print("Nilai a =", a)
+
+#assignment indirect
+b = a 
+print("Nilai b =", b)
+
