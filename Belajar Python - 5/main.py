85f67d4b5fa746c6e5613c7033a64d5d4dfbbf91
Danny A. J <63944195+dannyaj4@users.noreply.github.com>
2021-01-15 14:44:26 +0700

    Add files via upload

---
+++ Belajar Python - 5/main.py
@@ -0,0 +1,43 @@
+# Operator Aritmatika
+
+a = 3
+b = 4
+
+#Penambahan
+tambah = a + b
+print(a, " + ", b, "=", tambah)
+
+#Pengurangan 
+kurang = a - b
+print(a, " - ", b, "=", kurang)
+
+#Pembagian
+bagi = a / b
+print(a, " / ", b, "=", bagi)
+
+#Perkalian
+kali = a * b
+print(a, " * ", b, "=", kali)
+
+#Eksopnen / pangkat
+pangkat = a ** b
+print(a, " ** ", b, "=", pangkat)
+
+#Modulus
+modulus = a % b
+print(a, " * ", b, "=", modulus)
+
+#Operasi floor devision 
+o_fd = a // b
+print(a, " // ", b, "=", o_fd)
+
+# Prioritas Operasi aritmatika
+'''
+    1.	Kurung ()
+    2.	Eksponensial **, and friend / // % * 
+    3.	Tambah kurang, + - 
+'''
+
+
+
+
